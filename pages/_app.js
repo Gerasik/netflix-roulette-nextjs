b36880d0ef18a8811ccb7981761a98c13e244ad7
Gerasik <gerasik1992@gmail.com>
2020-05-21 18:38:38 +0300

    initiall template

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,23 @@
-import App from 'next/app'
 import React from 'react'
-import { Provider } from 'react-redux'
+import App from 'next/app'
+import {Provider} from 'react-redux'
 import withRedux from 'next-redux-wrapper'
-import withReduxSaga from 'next-redux-saga'
+import createStore from '../test/store/configure-store'
 
-import createStore from '../store'
-
-class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
+class ExampleApp extends App {
+  static async getInitialProps({Component, ctx}) {
     let pageProps = {}
 
     if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps({ ctx })
+      pageProps = await Component.getInitialProps({ctx})
     }
 
-    return { pageProps }
+    return {pageProps}
   }
 
   render() {
-    const { Component, pageProps, store } = this.props
+    const {Component, pageProps, store} = this.props
+
     return (
       <Provider store={store}>
         <Component {...pageProps} />
@@ -27,4 +26,4 @@ class MyApp extends App {
   }
 }
 
-export default withRedux(createStore)(withReduxSaga(MyApp))
+export default withRedux(createStore)(ExampleApp)
